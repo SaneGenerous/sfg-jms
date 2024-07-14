df40415716d90f647d28631476aa1209a9ef4026
Sane Generous <karim2k11@gmail.com>
2024-07-14 17:17:35 +0100

    Sending And Receiving Jms Messages.

--- src/main/java/tp/msk/sfgjms/sender/HelloSender.java
+++ src/main/java/tp/msk/sfgjms/sender/HelloSender.java
@@ -1,7 +1,13 @@
 package tp.msk.sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tp.msk.sfgjms.config.JmsConfig;
@@ -14,11 +20,10 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
-
-        System.out.println("I am sending a message");
 
         HelloWorldMessage message = HelloWorldMessage.builder()
                 .id(UUID.randomUUID())
@@ -27,6 +32,29 @@ public class HelloSender {
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-        System.out.println("Message Sent!");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage(){
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+        jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_MY_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "tp.msk.sfgjms.model.HelloWorldMessage");
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("Boom");
+                }
+            }
+        });
+
     }
 }
