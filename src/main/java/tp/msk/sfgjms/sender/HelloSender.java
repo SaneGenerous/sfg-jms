a2568007f20e5002d0a71d02c3d59fc6448e9c91
Sane Generous <karim2k11@gmail.com>
2024-07-14 15:41:57 +0100

    Sending Jms Messages.

---
+++ src/main/java/tp/msk/sfgjms/sender/HelloSender.java
@@ -0,0 +1,32 @@
+package tp.msk.sfgjms.sender;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import tp.msk.sfgjms.config.JmsConfig;
+import tp.msk.sfgjms.model.HelloWorldMessage;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+
+        System.out.println("I am sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message Sent!");
+    }
+}
